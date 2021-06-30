9580ed90cc35c24c2d92d7053242626f54df023d
Christian Stude <christian.stude@googlemail.com>
2021-06-30 11:03:47 +0200

    kleine Änderungen

--- scripts/docker-provision.sh
+++ scripts/docker-provision.sh
@@ -6,7 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Docker installieren
 apt-get update
 apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-get update
 apt-cache policy docker-ce
