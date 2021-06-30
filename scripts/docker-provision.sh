73150e3a0d848cc1bb781d0dc03f5580e6d274f2
Christian Stude <christian.stude@googlemail.com>
2021-06-30 13:20:48 +0200

    kleine Berichtigungen

--- scripts/docker-provision.sh
+++ scripts/docker-provision.sh
@@ -4,13 +4,15 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # Docker installieren
+apt-get -y install apt-transport-https ca-certificates curl \
+    gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | 
+    sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-get update
-apt-cache policy docker-ce
-apt-get -y install docker-ce
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
 systemctl enable --now docker
 adduser vagrant docker
